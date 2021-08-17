7382ef528778dd83a77f995157f281c1faab131a
Weilan <weilanc@gmail.com>
2021-08-17 00:40:36 -0400

    querying 10 positions with location info

--- src/queries.js
+++ src/queries.js
@@ -13,23 +13,45 @@ export const USER = gql`
 
 export const withUser = graphql(USER);
 
-export const DEVICE_LOCATIONS = gql`
-  query DeviceLocations {
-    allDevices {
-      nodes {
+// export const DEVICE_LOCATIONS = gql`
+//   query DeviceLocations {
+//     allDevices {
+//       nodes {
+//         id
+//         name
+//         batteryPercentage
+//         positionsByDeviceId(first: 10, orderBy: POSITION_AT_DESC) {
+//           nodes {
+//             id
+//             positionAt
+//             address
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+export const DEVICE_LOCATIONS=gql`
+query DeviceLocations {
+  allDevices {
+    nodes {
+      name
+      id
+      batteryPercentage
+      userByUserId {
         id
-        name
-        batteryPercentage
-        positionsByDeviceId(first: 1, orderBy: POSITION_AT_DESC) {
-          nodes {
-            id
-            positionAt
-            address
-          }
+      }
+      positionsByDeviceId (first: 10, orderBy: POSITION_AT_DESC)  {
+        nodes {
+          id
+          positionAt
+          latitude
+          longitude
+          address
         }
       }
     }
   }
-`;
-
+}`
 export const withDeviceLocations = graphql(DEVICE_LOCATIONS);
